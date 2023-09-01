1166f2220244571526aa60811e4ecfa4a1925853
mxf <547087214@qq.com>
2023-09-01 15:12:11 +0800

    修复空白屏

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,8 @@ import {visualizer } from 'rollup-plugin-visualizer' // 依赖分析
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:'/my-components-v3/',
-  base:'./',
+  base:'/my-components-v3/',
+  // base:'./',
   plugins: [
     vue(),
     vueSetupExtend(),
